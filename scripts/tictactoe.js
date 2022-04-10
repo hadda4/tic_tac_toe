8a21910245651cb848bb6fa7cd2a7fe7731682bc
hadda4 <th.develop@protonmail.com>
2022-04-10 15:16:37 +0200

    add computer random function for computer's turn

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -1,1 +1,28 @@
-console.log("TicTacToe - JS Files");
+console.log("TicTacToe - GAME");
+
+/* GLOBAL DEFINITIONS */
+const playOptions = ["Rock", "Paper", "Scissors"];
+
+
+/* MAIN */
+console.log(playOpponentsTurn());
+
+
+
+
+/* FUNCTIONS */
+
+/**
+ * play the Opponents Turn
+ * randomly Choosen (Rock, Paper, Scisscor)
+ * 
+ * @returns opponentTurn (Rock, Paper, Scisscor)
+ */
+function playOpponentsTurn(){
+    const randNum =  Math.floor(Math.random() * playOptions.length);
+    const opponentTurn = playOptions[randNum].toLocaleUpperCase();
+    // console.log(randNum, playOptions[randNum].toLocaleUpperCase());
+    return opponentTurn;
+}
+
+
