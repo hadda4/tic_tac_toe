158173f87b33cfdd3341fdfb69ea40913d72c5fc
hadda4 <th.develop@protonmail.com>
2022-04-10 15:45:41 +0200

    ask the player for his Selection and play against the computer Selection

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -5,24 +5,53 @@ const playOptions = ["Rock", "Paper", "Scissors"];
 
 
 /* MAIN */
-console.log(playOpponentsTurn());
-
+const playerSelection = getPlayerSelection();
+const opponentSelection = getOpponentsSelection();
+console.log("Player: " + playerSelection);
+console.log("\tagainst");
+console.log("Computer: " + opponentSelection);
+console.log(playRound(playerSelection, opponentSelection));
 
 
 
 /* FUNCTIONS */
 
 /**
- * play the Opponents Turn
- * randomly Choosen (Rock, Paper, Scisscor)
- * 
- * @returns opponentTurn (Rock, Paper, Scisscor)
+ * get a randomly choosen Selection 
+ * Rock, Paper or Scisscor
+ * for the opponentTurn
+ *  
+ * @returns String (Rock, Paper, Scisscor) in UPPERCASE
  */
-function playOpponentsTurn(){
-    const randNum =  Math.floor(Math.random() * playOptions.length);
+function getOpponentsSelection() {
+    const randNum = Math.floor(Math.random() * playOptions.length);
     const opponentTurn = playOptions[randNum].toLocaleUpperCase();
     // console.log(randNum, playOptions[randNum].toLocaleUpperCase());
     return opponentTurn;
 }
 
+/**
+ * get PlayerSelection
+ * Rock, Paper or Scisscor
+ * via Prompt
+ *  
+ * @returns String (Rock, Paper, Scisscor) in UPPERCASE
+ */
+function getPlayerSelection() {
+    return prompt("Choose ROCK, SCISSOR or PAPER", "").toLocaleUpperCase();
+}
+
+
+function playRound(playerSelection, opponentSelection) {
+    if (playerSelection == opponentSelection) {
+        return `TIE!\n${playerSelection} against ${opponentSelection}`
+    } else if (playerSelection == 'ROCK' && opponentSelection == 'PAPER' ||
+        playerSelection == 'SCISSCORS' && opponentSelection == 'ROCK' ||
+        playerSelection == 'PAPER' && opponentSelection == 'SCISSCORS'
+    ) {
+        return `YOU LOSE!\n${playerSelection} looses against ${opponentSelection}`
+    } else {
+        return `YOU WIN!\n${playerSelection} wins against ${opponentSelection}`
+    }
+}
 
